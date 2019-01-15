2542114779b159490def2a0a7731c28f8c4dbbc2
Kuba <kubanowikowski@gmail.com>
2019-01-15 15:00:38 +0100

    Read xml values

--- TrxToHtmlConverter/Program.cs
+++ TrxToHtmlConverter/Program.cs
@@ -3,13 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace TrxToHtmlConverter
 {
 	class Program
 	{
+
 		static void Main(string[] args)
 		{
+
+			XDocument doc = XDocument.Load("report.trx");
+			string xmlns = doc.Root.Name.Namespace.NamespaceName;
+			var results = doc.Element(XName.Get("TestRun", xmlns)).Element(XName.Get("Results", xmlns));
+			var summary = doc.Element(XName.Get("TestRun", xmlns)).Element(XName.Get("ResultSummary", xmlns)).Element(XName.Get("Counters", xmlns)).Attribute("total").Value;
+
+			Console.WriteLine(summary);
+
+
+			//var id =doc.Root.Element(XName.Get("TestRun"));
+			//var results = doc.Element("TestRun").Element("Results").Elements();
+			
+			//foreach (var child in results.Elements())
+			//{
+			//	Console.WriteLine(child.Attribute("testName").Value);
+			//}
+			
+			
 		}
 	}
 }
