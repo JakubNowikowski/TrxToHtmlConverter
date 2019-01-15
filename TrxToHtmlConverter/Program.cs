6259e45bdad328cb7ba8e67d80494569a71e57be
kasa4565 <45254527+kasa4565@users.noreply.github.com>
2019-01-15 16:09:47 +0100

    added first functionality to XmlReader

--- TrxToHtmlConverter/Program.cs
+++ TrxToHtmlConverter/Program.cs
@@ -13,23 +13,42 @@ namespace TrxToHtmlConverter
 		static void Main(string[] args)
 		{
 
-			XDocument doc = XDocument.Load("report.trx");
-			string xmlns = doc.Root.Name.Namespace.NamespaceName;
-			var results = doc.Element(XName.Get("TestRun", xmlns)).Element(XName.Get("Results", xmlns));
-			var summary = doc.Element(XName.Get("TestRun", xmlns)).Element(XName.Get("ResultSummary", xmlns)).Element(XName.Get("Counters", xmlns)).Attribute("total").Value;
+            //XDocument doc = XDocument.Load("report.trx");
+            //string xmlns = doc.Root.Name.Namespace.NamespaceName;
+            //var results = doc.Element(XName.Get("TestRun", xmlns)).Element(XName.Get("Results", xmlns));
+            //var summary = doc.Element(XName.Get("TestRun", xmlns)).Element(XName.Get("ResultSummary", xmlns)).Element(XName.Get("Counters", xmlns)).Attribute("total").Value;
 
-			Console.WriteLine(summary);
+            XmlReader doc = new XmlReader("report.trx");
+
+            Console.WriteLine(doc.GetTotal() + "\n");
+            Console.WriteLine(doc.GetExecuted() + "\n");
+            Console.WriteLine(doc.GetPassed() + "\n");
+            Console.WriteLine(doc.GetFailed() + "\n");
+            Console.WriteLine(doc.GetError() + "\n");
+            Console.WriteLine(doc.GetTimeout() + "\n");
+            Console.WriteLine(doc.GetAborted() + "\n");
+            Console.WriteLine(doc.GetInconclusive() + "\n");
+            Console.WriteLine(doc.GetPassedButRunAborted() + "\n");
+            Console.WriteLine(doc.GetNotRunnable() + "\n");
+            Console.WriteLine(doc.GetNotExecuted() + "\n");
+            Console.WriteLine(doc.GetDisconnectedl() + "\n");
+            Console.WriteLine(doc.GetWarning() + "\n");
+            Console.WriteLine(doc.GetCompleted() + "\n");
+            Console.WriteLine(doc.GetInProgress() + "\n");
+            Console.WriteLine(doc.GetPending() + "\n");
 
 
-			//var id =doc.Root.Element(XName.Get("TestRun"));
-			//var results = doc.Element("TestRun").Element("Results").Elements();
-			
-			//foreach (var child in results.Elements())
-			//{
-			//	Console.WriteLine(child.Attribute("testName").Value);
-			//}
-			
-			
-		}
-	}
+            Console.ReadKey();
+
+            //var id =doc.Root.Element(XName.Get("TestRun"));
+            //var results = doc.Element("TestRun").Element("Results").Elements();
+
+            //foreach (var child in results.Elements())
+            //{
+            //	Console.WriteLine(child.Attribute("testName").Value);
+            //}
+
+
+        }
+    }
 }
--- TrxToHtmlConverter/XmlReader.cs
+++ TrxToHtmlConverter/XmlReader.cs
@@ -3,11 +3,99 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace TrxToHtmlConverter
 {
 	class XmlReader
 	{
-		
-	}
+        XDocument doc;
+        string xmlns;
+
+        public XmlReader(string file)
+        {
+            doc = XDocument.Load(file);
+            xmlns = doc.Root.Name.Namespace.NamespaceName;
+        }
+
+        public string GetTotal()
+        {
+            return doc.Element(XName.Get("TestRun", xmlns)).Element(XName.Get("ResultSummary", xmlns)).Element(XName.Get("Counters", xmlns)).Attribute("total").Value;
+        }
+
+        public string GetExecuted()
+        {
+            return doc.Element(XName.Get("TestRun", xmlns)).Element(XName.Get("ResultSummary", xmlns)).Element(XName.Get("Counters", xmlns)).Attribute("executed").Value;
+        }
+
+        public string GetPassed()
+        {
+            return doc.Element(XName.Get("TestRun", xmlns)).Element(XName.Get("ResultSummary", xmlns)).Element(XName.Get("Counters", xmlns)).Attribute("passed").Value;
+        }
+
+        public string GetFailed()
+        {
+            return doc.Element(XName.Get("TestRun", xmlns)).Element(XName.Get("ResultSummary", xmlns)).Element(XName.Get("Counters", xmlns)).Attribute("failed").Value;
+        }
+
+        public string GetError()
+        {
+            return doc.Element(XName.Get("TestRun", xmlns)).Element(XName.Get("ResultSummary", xmlns)).Element(XName.Get("Counters", xmlns)).Attribute("error").Value;
+        }
+
+        public string GetTimeout()
+        {
+            return doc.Element(XName.Get("TestRun", xmlns)).Element(XName.Get("ResultSummary", xmlns)).Element(XName.Get("Counters", xmlns)).Attribute("timeout").Value;
+        }
+
+        public string GetAborted()
+        {
+            return doc.Element(XName.Get("TestRun", xmlns)).Element(XName.Get("ResultSummary", xmlns)).Element(XName.Get("Counters", xmlns)).Attribute("aborted").Value;
+        }
+
+        public string GetInconclusive()
+        {
+            return doc.Element(XName.Get("TestRun", xmlns)).Element(XName.Get("ResultSummary", xmlns)).Element(XName.Get("Counters", xmlns)).Attribute("inconclusive").Value;
+        }
+
+        public string GetPassedButRunAborted()
+        {
+            return doc.Element(XName.Get("TestRun", xmlns)).Element(XName.Get("ResultSummary", xmlns)).Element(XName.Get("Counters", xmlns)).Attribute("passedButRunAborted").Value;
+        }
+
+        public string GetNotRunnable()
+        {
+            return doc.Element(XName.Get("TestRun", xmlns)).Element(XName.Get("ResultSummary", xmlns)).Element(XName.Get("Counters", xmlns)).Attribute("notRunnable").Value;
+        }
+
+        public string GetNotExecuted()
+        {
+            return doc.Element(XName.Get("TestRun", xmlns)).Element(XName.Get("ResultSummary", xmlns)).Element(XName.Get("Counters", xmlns)).Attribute("notExecuted").Value;
+        }
+
+        public string GetDisconnectedl()
+        {
+            return doc.Element(XName.Get("TestRun", xmlns)).Element(XName.Get("ResultSummary", xmlns)).Element(XName.Get("Counters", xmlns)).Attribute("disconnected").Value;
+        }
+
+        public string GetWarning()
+        {
+            return doc.Element(XName.Get("TestRun", xmlns)).Element(XName.Get("ResultSummary", xmlns)).Element(XName.Get("Counters", xmlns)).Attribute("warning").Value;
+        }
+
+        public string GetCompleted()
+        {
+            return doc.Element(XName.Get("TestRun", xmlns)).Element(XName.Get("ResultSummary", xmlns)).Element(XName.Get("Counters", xmlns)).Attribute("completed").Value;
+        }
+
+        public string GetInProgress()
+        {
+            return doc.Element(XName.Get("TestRun", xmlns)).Element(XName.Get("ResultSummary", xmlns)).Element(XName.Get("Counters", xmlns)).Attribute("inProgress").Value;
+        }
+
+        public string GetPending()
+        {
+            return doc.Element(XName.Get("TestRun", xmlns)).Element(XName.Get("ResultSummary", xmlns)).Element(XName.Get("Counters", xmlns)).Attribute("pending").Value;
+        }
+    }
 }
