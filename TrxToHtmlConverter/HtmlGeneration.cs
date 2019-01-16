3eafa12c0728c1b2639d2e4c62fc0c0b199d3a6f
andrzejjaroslawkrupa <andrzej.jaroslaw.krupa@gmail.com>
2019-01-16 10:24:50 +0100

    Added export to file, head and style creation of html

---
+++ TrxToHtmlConverter/HtmlGeneration.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI;
+
+namespace TrxToHtmlConverter
+{
+    class HtmlGeneration
+    {
+        private string _Path;
+        public HtmlGeneration(string path)
+        {
+            _Path = path;
+        }
+        public void Generation()
+        {
+            StringWriter stringWriter = new StringWriter();
+            using (HtmlTextWriter writer = new HtmlTextWriter(stringWriter))
+            {
+                writer.WriteFullBeginTag("head");
+                writer.WriteFullBeginTag("style");
+                writer.WriteLine("body");
+                writer.WriteLine("{");
+                writer.WriteStyleAttribute("font-family", "arial");
+                writer.WriteLine("}");
+                writer.WriteLine("div.header1");
+                writer.WriteLine("{");
+                writer.WriteStyleAttribute("background-color", "#3F51B5");
+                writer.WriteStyleAttribute("height", "84px");
+                writer.WriteStyleAttribute("position", "relative");
+                writer.WriteLine("}");
+                writer.WriteLine("div.header1 h1");
+                writer.WriteLine("{");
+                writer.WriteStyleAttribute("position", "absolute");
+                writer.WriteStyleAttribute("color", "#FFFFFF");
+                writer.WriteStyleAttribute("margin", "0");
+                writer.WriteStyleAttribute("top", "50%");
+                writer.WriteStyleAttribute("left", "84px");
+                writer.WriteStyleAttribute("transform", "translate(0, -50%)");
+                writer.WriteLine("}");
+                writer.WriteEndTag("style");
+                writer.WriteEndTag("head");
+            }
+            ExportToFile(stringWriter);
+        }
+
+        private void ExportToFile(StringWriter fileContent)
+        {
+            StreamWriter fw = new StreamWriter(_Path);
+            fw.Write(fileContent.ToString());
+            fw.Close();
+        }
+    }
+}
--- TrxToHtmlConverter/Program.cs
+++ TrxToHtmlConverter/Program.cs
@@ -18,27 +18,8 @@ namespace TrxToHtmlConverter
             //var results = doc.Element(XName.Get("TestRun", xmlns)).Element(XName.Get("Results", xmlns));
             //var summary = doc.Element(XName.Get("TestRun", xmlns)).Element(XName.Get("ResultSummary", xmlns)).Element(XName.Get("Counters", xmlns)).Attribute("total").Value;
 
-            XmlReader doc = new XmlReader("report.trx");
-
-            Console.WriteLine(doc.GetTotal() + "\n");
-            Console.WriteLine(doc.GetExecuted() + "\n");
-            Console.WriteLine(doc.GetPassed() + "\n");
-            Console.WriteLine(doc.GetFailed() + "\n");
-            Console.WriteLine(doc.GetError() + "\n");
-            Console.WriteLine(doc.GetTimeout() + "\n");
-            Console.WriteLine(doc.GetAborted() + "\n");
-            Console.WriteLine(doc.GetInconclusive() + "\n");
-            Console.WriteLine(doc.GetPassedButRunAborted() + "\n");
-            Console.WriteLine(doc.GetNotRunnable() + "\n");
-            Console.WriteLine(doc.GetNotExecuted() + "\n");
-            Console.WriteLine(doc.GetDisconnectedl() + "\n");
-            Console.WriteLine(doc.GetWarning() + "\n");
-            Console.WriteLine(doc.GetCompleted() + "\n");
-            Console.WriteLine(doc.GetInProgress() + "\n");
-            Console.WriteLine(doc.GetPending() + "\n");
-
-
-            Console.ReadKey();
+            HtmlGeneration Html = new HtmlGeneration(@"C:\Users\ivakrupa\Downloads\xd.html");
+            Html.Generation();
 
             //var id =doc.Root.Element(XName.Get("TestRun"));
             //var results = doc.Element("TestRun").Element("Results").Elements();
