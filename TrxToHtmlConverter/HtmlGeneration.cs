0ad5f94fd82b1c38aa75399b6d71e83b2bb571da
andrzejjaroslawkrupa <andrzej.jaroslaw.krupa@gmail.com>
2019-01-22 13:28:07 +0100

    Added creation and filling of failed test table

--- TrxToHtmlConverter/HtmlGeneration.cs
+++ TrxToHtmlConverter/HtmlGeneration.cs
@@ -9,46 +9,69 @@ using HtmlAgilityPack;
 
 namespace TrxToHtmlConverter
 {
-	class HtmlGeneration
-	{
-		private string _OutputPath;
-		private string _TemplatePath;
-		private string _TrxFilePath;
-		private XmlReader _XmlReader;
-		private TestLoadResult _TestLoadResult;
-		public HtmlGeneration(string trxFilePath, string outputPath)
-		{
-			_OutputPath = outputPath;
-			_TemplatePath = @"../../template.html";
-			_TrxFilePath = trxFilePath;
-		}
-		public void Generation()
-		{
-			_XmlReader = InitializeTrxData();
-			var document = LoadTemplate(_TemplatePath);
+    class HtmlGeneration
+    {
+        private string _OutputPath;
+        private string _TemplatePath;
+        private string _TrxFilePath;
+        private XmlReader _XmlReader;
+        private TestLoadResult _TestLoadResult;
+        public HtmlGeneration(string trxFilePath, string outputPath)
+        {
+            _OutputPath = outputPath;
+            _TemplatePath = @"../../template.html";
+            _TrxFilePath = trxFilePath;
+        }
+        public void Generation()
+        {
+            _XmlReader = InitializeTrxData();
+            var document = LoadTemplate(_TemplatePath);
 
-			HtmlNodeCollection htmlNodes = new HtmlNodeCollection(document.DocumentNode);
+            HtmlNodeCollection htmlNodes = new HtmlNodeCollection(document.DocumentNode);
 
             document = ChangeNameOfDocument(document, _TestLoadResult.totalTestsProp.TestCategory);
             document = ReplaceAllTotalValues(document);
             document = ReplaceAllRunTimeSummaryValues(document);
-
-            var tableTestCase = document.DocumentNode.SelectSingleNode("/html/body")
-                .Element("div").Elements("div").First(d => d.Id == "test")
-                .Element("div").Elements("table").First(d => d.Id == "ReportsTable").Element("tbody").
-                Elements("tr").First(d => d.Id == "TestsContainer").Element("td").Element("table").Element("tbody");
-           
-            CreateTestCaseTableRows(tableTestCase);
+            document = FillFailedTestsTable(document);
+            
 
             document.DocumentNode.AppendChildren(htmlNodes);
 
-			ExportToFile(document.DocumentNode.InnerHtml);
-		}
+            ExportToFile(document.DocumentNode.InnerHtml);
+        }
+
+        private HtmlDocument FillFailedTestsTable(HtmlDocument doc)
+        {
+            var tableTestCase = doc.DocumentNode.SelectSingleNode("/html/body")
+                .Element("div").Elements("div").First(d => d.Id == "test")
+                .Element("div").Elements("table").First(d => d.Id == "FailedTestTable").Element("tbody")
+                .Elements("tr").First(d => d.Id == "TestsContainer").Element("td").Element("table").Element("tbody");
+
+            doc = ChangeNumberOfAllFailedTests(doc, _TestLoadResult.totalTestsProp.Failed);
+            var failedResults = PredicateCreator(t => t.Result, "Failed");
+            CreateTestCaseTableRows(tableTestCase, failedResults);
+
+            return doc;
+        }
+
+        private HtmlDocument ChangeNumberOfAllFailedTests(HtmlDocument doc, string number)
+        {
+            var failedTableTitleNode = doc.DocumentNode.SelectSingleNode("html/body")
+                .Element("div").Elements("div").First(d => d.Id == "test")
+                .Element("div").Elements("table").First(d => d.Id == "FailedTestTable")
+                .Element("tbody").Elements("tr").First(t => t.Id == "FailedTestsHeader")
+                .Elements("td").First(t => t.Id == "number");
+            var valueNode = failedTableTitleNode.InnerHtml;
+            valueNode = valueNode.Replace("VALUE", number);
+            failedTableTitleNode.InnerHtml = HtmlDocument.HtmlEncode(valueNode);
+
+            return doc;
+        }
 
         private HtmlDocument ChangeNameOfDocument(HtmlDocument doc, string nameValue)
         {
-            var titleNode = doc.DocumentNode.SelectSingleNode("/html/body").
-                Element("div").Elements("div").First(d => d.Id == "header");
+            var titleNode = doc.DocumentNode.SelectSingleNode("/html/body")
+                .Element("div").Elements("div").First(d => d.Id == "header");
             var valueNode = titleNode.Element("h1").InnerHtml;
             valueNode = valueNode.Replace("TEMPLATE", nameValue);
             titleNode.Element("h1").InnerHtml = HtmlDocument.HtmlEncode(valueNode);
@@ -102,16 +125,21 @@ namespace TrxToHtmlConverter
             return doc;
         }
 
-        private void CreateTestCaseTableRows(HtmlNode tableTestCase)
+        public Func<Test, bool> PredicateCreator<T>(Func<Test, T> selector, T expected) where T : IEquatable<T>
         {
-            foreach (Test test in _TestLoadResult.tests)
+            return t => selector(t).Equals(expected);
+        }
+
+        private void CreateTestCaseTableRows(HtmlNode tableTestCase, Func<Test, bool> func)
+        {
+            foreach (Test test in _TestLoadResult.tests.Where(func))
             {
                 HtmlNode tableRowTestCase = HtmlNode.CreateNode("<tr class=\"Test\"></tr>");
 
                 tableTestCase.AppendChild(tableRowTestCase);
                 tableTestCase = tableTestCase.LastChild;
 
-                tableRowTestCase = HtmlNode.CreateNode("<th scope=\"row\" class=\"column1\">7/28/2014 9:47:32 PM</th>");
+                tableRowTestCase = HtmlNode.CreateNode($"<th scope=\"row\" class=\"column1\"></th>");
                 tableTestCase.AppendChild(tableRowTestCase);
 
                 tableRowTestCase = HtmlNode.CreateNode($"<td class=\"{test.Result}\">{CreateColoredResult(test.Result)}</td>");
@@ -123,33 +151,33 @@ namespace TrxToHtmlConverter
                 tableRowTestCase = HtmlNode.CreateNode("<td class=\"Message\"></td>");
                 tableTestCase.AppendChild(tableRowTestCase);
 
-                tableRowTestCase = HtmlNode.CreateNode("<td class=\"Message\"></td>");
+                tableRowTestCase = HtmlNode.CreateNode($"<td class=\"Message\">{test.ID}</td>");
                 tableTestCase.AppendChild(tableRowTestCase);
 
-                tableRowTestCase = HtmlNode.CreateNode("<td class=\"Message\">138.6413 ms</td>");
+                tableRowTestCase = HtmlNode.CreateNode("<td class=\"Message\"></td>");
                 tableTestCase.AppendChild(tableRowTestCase);
             }
 
 
         }
-			
-		private void GetMethods(IEnumerable<Test> methodList, HtmlNodeCollection htmlNode)
-		{
-			foreach (Test method in methodList)
-			{
-				HtmlNode testNodeMethod = HtmlNode.CreateNode($"<ul>" +
-				$"<li>{CreateColoredResult(method.Result)}" +
-				$"<b>Method Name</b> <br> {method.MethodName}</li>" +
-				$"</ul>");
-				htmlNode.Add(testNodeMethod);
-			}
-		}
+
+        private void GetMethods(IEnumerable<Test> methodList, HtmlNodeCollection htmlNode)
+        {
+            foreach (Test method in methodList)
+            {
+                HtmlNode testNodeMethod = HtmlNode.CreateNode($"<ul>" +
+                $"<li>{CreateColoredResult(method.Result)}" +
+                $"<b>Method Name</b> <br> {method.MethodName}</li>" +
+                $"</ul>");
+                htmlNode.Add(testNodeMethod);
+            }
+        }
 
         public string CreateClassFilter(List<string> listOfClasses)
         {
-            string filter="<select>";
+            string filter = "<select>";
 
-            foreach(string e in listOfClasses)
+            foreach (string e in listOfClasses)
             {
                 filter += $"<option>{e}</option>";
             }
@@ -173,49 +201,49 @@ namespace TrxToHtmlConverter
                 case "Inconclusive": color = "blue"; break;
                 case "Warning": color = "orange"; break;
             }
-            
+
             return $"<font color={color} size=4><strong>{result}</strong></font><br>";
         }
 
-		private TimeSpan TestsDuration(DateTime startTime, DateTime stopTime)
-		{
-			return stopTime - startTime;
-		}
+        private TimeSpan TestsDuration(DateTime startTime, DateTime stopTime)
+        {
+            return stopTime - startTime;
+        }
 
-		private XmlReader InitializeTrxData()
-		{
-			XmlReader xmlReader = new XmlReader(_TrxFilePath);
-			_TestLoadResult = xmlReader.CreateTestLoadResult();
+        private XmlReader InitializeTrxData()
+        {
+            XmlReader xmlReader = new XmlReader(_TrxFilePath);
+            _TestLoadResult = xmlReader.CreateTestLoadResult();
 
-			return xmlReader;
-		}
+            return xmlReader;
+        }
 
-		private string TotalResultString()
-		{
-			if ((Convert.ToInt32(_TestLoadResult.totalTestsProp.Passed) / Convert.ToInt32(GetTotalTestNumber())) == 1)
-				return "Passed";
-			else
-				return "Failed";
-		}
+        private string TotalResultString()
+        {
+            if ((Convert.ToInt32(_TestLoadResult.totalTestsProp.Passed) / Convert.ToInt32(GetTotalTestNumber())) == 1)
+                return "Passed";
+            else
+                return "Failed";
+        }
 
-		private string GetTotalTestNumber()
-		{
-			return _TestLoadResult.totalTestsProp.Total;
-		}
+        private string GetTotalTestNumber()
+        {
+            return _TestLoadResult.totalTestsProp.Total;
+        }
 
-		private void ExportToFile(string fileContent)
-		{
-			StreamWriter fw = new StreamWriter(_OutputPath);
-			fw.Write(fileContent);
-			fw.Close();
-		}
+        private void ExportToFile(string fileContent)
+        {
+            StreamWriter fw = new StreamWriter(_OutputPath);
+            fw.Write(fileContent);
+            fw.Close();
+        }
 
-		private HtmlDocument LoadTemplate(string templatePath)
-		{
-			var doc = new HtmlDocument();
-			doc.Load(templatePath, Encoding.UTF8);
+        private HtmlDocument LoadTemplate(string templatePath)
+        {
+            var doc = new HtmlDocument();
+            doc.Load(templatePath, Encoding.UTF8);
 
-			return doc;
-		}
-	}
+            return doc;
+        }
+    }
 }
